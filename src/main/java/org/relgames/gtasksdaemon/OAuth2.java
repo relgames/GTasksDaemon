075a7b082ed897624bd2a6c57541498ef2d7847c
Oleg Poleshuk <relgames@gmail.com>
2011-09-19 00:43:20 +0200

    Google API: works fine

--- src/main/java/org/relgames/gtasksdaemon/OAuth2.java
+++ src/main/java/org/relgames/gtasksdaemon/OAuth2.java
@@ -4,6 +4,9 @@ import com.google.api.client.auth.oauth2.draft10.AccessProtectedResource;
 import com.google.api.client.auth.oauth2.draft10.AccessTokenRequest.AuthorizationCodeGrant;
 import com.google.api.client.auth.oauth2.draft10.AccessTokenResponse;
 import com.google.api.client.auth.oauth2.draft10.AuthorizationRequestUrl;
+import com.google.api.client.googleapis.auth.oauth2.draft10.GoogleAccessProtectedResource;
+import com.google.api.client.googleapis.auth.oauth2.draft10.GoogleAccessTokenRequest;
+import com.google.api.client.googleapis.auth.oauth2.draft10.GoogleAuthorizationRequestUrl;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
@@ -15,7 +18,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.bridge.SLF4JBridgeHandler;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.logging.Handler;
 import java.util.logging.Level;
 import java.util.logging.LogManager;
@@ -25,26 +30,21 @@ import java.util.logging.LogManager;
  */
 public class OAuth2 {
     private static final String SCOPE = "https://www.googleapis.com/auth/tasks";
-    private static final String AUTH_URL = "https://accounts.google.com/o/oauth2/auth";
     private static final String REDIRECT_URL = "urn:ietf:wg:oauth:2.0:oob";
 
-    private static final String CLIENT_ID = "495463767429.apps.googleusercontent.com";
-    private static final String CLIENT_SECRET = "yNuz9Iq1M4JZm61TAb2sVTu0";
+    private static final String CLIENT_ID = "495463767429-cr99g3fnuf4u0pnq8qts4j9ob84q7ims.apps.googleusercontent.com";
+    private static final String CLIENT_SECRET = "oHmMQ8qbFxiyFxty6RoH_7al";
 
     private static final HttpTransport TRANSPORT = new NetHttpTransport();
     private static final JsonFactory JSON_FACTORY = new JacksonFactory();
 
     public static void requestCode() throws IOException {
-        AuthorizationRequestUrl builder = new AuthorizationRequestUrl(AUTH_URL, CLIENT_ID);
-        builder.redirectUri = REDIRECT_URL;
-        builder.scope = SCOPE;
-
-        String authorizationUrl = builder.build();
+        String authorizationUrl = new GoogleAuthorizationRequestUrl(CLIENT_ID, REDIRECT_URL, SCOPE).build();
         System.out.println("Go to the following link in your browser:");
         System.out.println(authorizationUrl);
     }
 
-    private static final String CODE = "4/hMGkDFvZyGAGI2S28qydorJb4FmI";
+    private static final String CODE = "4/CCYTP9PC6BcrdzJWBSq89PrN0sj2";
 
     private static final Logger log = LoggerFactory.getLogger(OAuth2.class);
 
@@ -59,23 +59,27 @@ public class OAuth2 {
 
         log.debug("OAuth2");
 
-        //requestCode();
+/*
+        requestCode();
 
-        AccessTokenResponse response = new AuthorizationCodeGrant(
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("What is the authorization code?");
+        String code = in.readLine();
+*/
+
+
+        AccessTokenResponse response = new GoogleAccessTokenRequest.GoogleAuthorizationCodeGrant(
                 TRANSPORT,
                 JSON_FACTORY,
-                AUTH_URL,
                 CLIENT_ID,
                 CLIENT_SECRET,
                 CODE,
                 REDIRECT_URL).execute();
 
-        AccessProtectedResource accessProtectedResource = new AccessProtectedResource(
+        AccessProtectedResource accessProtectedResource = new GoogleAccessProtectedResource(
                 response.accessToken,
-                AccessProtectedResource.Method.AUTHORIZATION_HEADER,
                 TRANSPORT,
                 JSON_FACTORY,
-                AUTH_URL,
                 CLIENT_ID,
                 CLIENT_SECRET,
                 response.refreshToken);
