14157e9cda364037935f1d506f69423272bd3096
Oleg Poleshuk <relgames@gmail.com>
2011-09-20 18:18:33 +0200

    Added retry in case of exceptions

--- src/main/java/org/relgames/gtasksdaemon/QuartzJob.java
+++ src/main/java/org/relgames/gtasksdaemon/QuartzJob.java
@@ -10,15 +10,33 @@ import org.slf4j.LoggerFactory;
 /**
  * @author Oleg Poleshuk
  */
-public class QuartzJob implements Job{
+public class QuartzJob implements Job {
     public static DateTime lastExecution;
 
     private final Logger log = LoggerFactory.getLogger(this.getClass());
+    private static final int TRY_COUNT = 5;
 
     @Override
     public void execute(JobExecutionContext context) throws JobExecutionException {
-        log.info("Calling MainService.process()");
-        MainService.process();
-        lastExecution = new DateTime();
+        int count = 0;
+        while (true) {
+            try {
+                count++;
+                log.info("Calling MainService.process(), attempt {}", count);
+                MainService.process();
+                lastExecution = new DateTime();
+                break;
+            } catch (Exception e) {
+                log.error("Exception", e);
+                if (count >= TRY_COUNT) {
+                    log.warn("Reached TRY_COUNT, exiting");
+                    break;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        new QuartzJob().execute(null);
     }
 }
